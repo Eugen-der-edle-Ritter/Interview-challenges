75893256ff5b0f1f57e77aa021cd4b6564486b96
Eugen-der-edle-Ritter <66753924+Eugen-der-edle-Ritter@users.noreply.github.com>
2020-09-23 19:36:36 +0300

    Update and rename #2_palindromeCounter.js to palindromeCounter.js

---
+++ palindromeCounter.js
@@ -0,0 +1,19 @@
+/*
+Нужно описать функцию, которая подсчитывает количество слов палиндромов и слов непалиндромов в строке.  На вход функция принимает строку - набор слов, разделенных пробелами. Возвращать функция должна объект состоящий из двух числовых полей - количество словпалиндромов и слов не-палиндромов.
+*/
+
+function isPalindrome(str) {
+    const wordArray = str.split(' ');
+    let counterObject = {palindromeCount: 0, notPalindromeCount: 0};
+    
+    wordArray.forEach(el => {
+        if(el.toLowerCase().split('').reverse().join('') === el.toLowerCase()) {
+            counterObject.palindromeCount += 1;
+        }
+        else {
+            counterObject.notPalindromeCount += 1;
+        }
+    });
+    
+    return counterObject;
+}
